a9ee194c36f83b0e58240d7c7e5e0381586e548e
taku.kotegawa <taku.kotegawa@gmail.com>
2024-08-11 02:08:31 +0900

    initial commit

---
+++ src/main/java/com/example/multiple_dispatcher_example/MultipleDispatcherExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiple_dispatcher_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDispatcherExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDispatcherExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multiple_dispatcher_example/MultipleDispatcherExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiple_dispatcher_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDispatcherExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
